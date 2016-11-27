0ec92ea375dc40548785963f1642aa4190d1c374
Ayuna NYC <ayunanyc@Ayunas-MacBook-Pro.local>
2016-11-27 18:31:41 -0500

    Initial Commit

---
+++ BNRPhotorama/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BNRPhotorama
+//
+//  Created by Ayuna NYC on 11/27/16.
+//  Copyright © 2016 Ayuna NYC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
