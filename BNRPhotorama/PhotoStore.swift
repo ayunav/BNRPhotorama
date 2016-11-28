4737129dbeec45682a57d3d094145ba679aaee36
Ayuna NYC <ayunanyc@Ayunas-MacBook-Pro.local>
2016-11-28 16:22:03 -0500

    Add PhotoStore class to fetch data from the API

--- BNRPhotorama/PhotoStore.swift
+++ BNRPhotorama/PhotoStore.swift
@@ -7,3 +7,40 @@
 //
 
 import Foundation
+
+class PhotoStore
+{
+    let session: URLSession =
+    {
+        let config = URLSessionConfiguration.default
+        return URLSession(configuration: config)
+    }()
+ 
+    func fetchRecentPhotos()
+    {
+        let url = FlickrAPI.recentPhotosURL()
+        let request = URLRequest(url: url!)
+        
+        let task = session.dataTask(with: request)
+            { (data, response, error) -> Void in
+            
+                if let jsonData = data
+                {
+                    if let jsonString = String(data: jsonData, encoding: String.Encoding.utf8)
+                    {
+                        print(jsonString)
+                    }
+                }
+                else if let requestError = error
+                {
+                    print("Error fetching recent photos: \(requestError)")
+                }
+                else
+                {
+                    print("Unxpected error with the request")
+                }
+            }
+        task.resume()
+    }
+
+}
--- BNRPhotorama/PhotosViewController.swift
+++ BNRPhotorama/PhotosViewController.swift
@@ -12,4 +12,12 @@ import UIKit
 class PhotosViewController: UIViewController {
     
     @IBOutlet var imageView: UIImageView!
+    
+    var store: PhotoStore!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        store.fetchRecentPhotos()
+    }
 }
