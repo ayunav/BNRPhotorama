fdcef3626aa41de4745996357355904b8d8676a1
Ayuna NYC <ayunanyc@Ayunas-MacBook-Pro.local>
2016-11-28 15:26:47 -0500

    Add FlickrAPI API Manager struct

---
+++ BNRPhotorama/PhotoStore.swift
@@ -0,0 +1,9 @@
+//
+//  PhotoStore.swift
+//  BNRPhotorama
+//
+//  Created by Ayuna NYC on 11/28/16.
+//  Copyright © 2016 Ayuna NYC. All rights reserved.
+//
+
+import Foundation
---
+++ BNRPhotorama/PhotosViewController.swift
@@ -0,0 +1,15 @@
+//
+//  PhotosViewController.swift
+//  BNRPhotorama
+//
+//  Created by Ayuna NYC on 11/27/16.
+//  Copyright © 2016 Ayuna NYC. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PhotosViewController: UIViewController {
+    
+    @IBOutlet var imageView: UIImageView!
+}
